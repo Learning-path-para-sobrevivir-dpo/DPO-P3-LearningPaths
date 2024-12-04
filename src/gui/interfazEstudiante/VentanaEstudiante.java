e0f40521c66841ea7a3dc1d183d6d4c7f3b0a5c0
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-12-03 21:22:58 -0500

    Constructores Ventana Estudiante y Profesor

--- src/gui/interfazEstudiante/VentanaEstudiante.java
+++ src/gui/interfazEstudiante/VentanaEstudiante.java
@@ -2,6 +2,21 @@ package gui.interfazEstudiante;
 
 import javax.swing.JFrame;
 
+import gui.GUIManejoDatos;
+import gui.VentanaPrincipal;
+import modelo.Estudiante;
+
 public class VentanaEstudiante extends JFrame {
+	
+	private VentanaPrincipal ventanaInicio;
+	
+	private Estudiante est;
+	private GUIManejoDatos datos;
+	
+	public VentanaEstudiante(Estudiante e, GUIManejoDatos datos, VentanaPrincipal ventanaInicio) {
+		this.datos = datos;
+		this.est = e;
+		this.ventanaInicio = ventanaInicio;
+	}
 
 }
--- src/gui/interfazProfesor/VentanaProfesor.java
+++ src/gui/interfazProfesor/VentanaProfesor.java
@@ -2,6 +2,22 @@ package gui.interfazProfesor;
 
 import javax.swing.JFrame;
 
+import gui.GUIManejoDatos;
+import gui.VentanaPrincipal;
+import modelo.Profesor;
+
 public class VentanaProfesor extends JFrame {
+	
+	private VentanaPrincipal ventanaInicio;
+	
+	private Profesor prof;
+	private GUIManejoDatos datos;
+	
+	public VentanaProfesor(Profesor p, GUIManejoDatos datos, VentanaPrincipal ventanaInicio) {
+		// TODO Auto-generated constructor stub
+		this.prof = p;
+		this.datos = datos;
+		this.ventanaInicio = ventanaInicio;
+	}
 
 }
