184a13216e67ea6d46f4d9c7b23712d91b4cea61
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-12-08 17:14:21 -0500

    Merge branch 'master' of https://github.com/Learning-path-para-sobrevivir-dpo/DPO-P3-LearningPaths.git

--- src/gui/interfazProfesor/seguimiento/verActividad/PanelBotonesCalificar.java
+++ src/gui/interfazProfesor/seguimiento/verActividad/PanelBotonesCalificar.java
@@ -1,6 +1,7 @@
 package gui.interfazProfesor.seguimiento.verActividad;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,6 +21,19 @@ public class PanelBotonesCalificar extends JPanel implements ActionListener {
 	public PanelBotonesCalificar(VentanaCalificarActividad ventanaCalificar) {
 		this.ventanaCalificar = ventanaCalificar;
 		setLayout(new FlowLayout());
+		
+		butCancelar = new JButton("Cancelar");
+		butCancelar.setActionCommand(CANCELAR);
+		butCancelar.addActionListener(this);
+		butCancelar.setFont(new Font("Calibri", Font.BOLD, 15));
+		
+		butCalificar = new JButton("Calificar");
+		butCalificar.setActionCommand(CALIFICAR);
+		butCalificar.addActionListener(this);
+		butCalificar.setFont(new Font("Calibri", Font.BOLD, 15));
+		
+		add(butCancelar);
+		add(butCalificar);
 	}
 
 
