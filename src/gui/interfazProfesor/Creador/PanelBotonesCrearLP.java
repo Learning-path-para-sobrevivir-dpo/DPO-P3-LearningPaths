0d0b8507cc036d6e4029f7a5215f3bf61a39ae65
Sara <admin@Saras-MacBook.local>
2024-12-08 14:53:13 -0500

    cambios

--- src/gui/interfazProfesor/Creador/PanelBotonesCrearLP.java
+++ src/gui/interfazProfesor/Creador/PanelBotonesCrearLP.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
